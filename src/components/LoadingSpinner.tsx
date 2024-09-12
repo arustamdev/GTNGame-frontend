46643b1016ad635fec9a6f6e2b85df8a78c9ed72
arustamdev <arustamdev@gmail.com>
2024-09-12 03:49:04 +0200

    added way to give a caption to loading spinner

--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -1,17 +1,28 @@
-import { Center, Spinner } from '@chakra-ui/react';
+import { Box, Center, Spinner, VStack } from '@chakra-ui/react';
 
-function LoadingSpinner() {
+interface LoadingSpinnerProps {
+  caption?: string;
+}
+
+function LoadingSpinner({ caption }: LoadingSpinnerProps) {
   return (
     <Center h={'100%'}>
-      <Spinner
-        w={'7rem'}
-        h={'7rem'}
-        thickness="12px"
-        speed="0.65s"
-        emptyColor="gray.200"
-        color="blue.500"
-        size="xl"
-      />
+      <VStack>
+        <Spinner
+          w={'7rem'}
+          h={'7rem'}
+          thickness="12px"
+          speed="0.65s"
+          emptyColor="gray.200"
+          color="blue.500"
+          size="xl"
+        />
+        {caption && (
+          <Box as="span" fontWeight="bold" fontSize="x-large">
+            {caption}
+          </Box>
+        )}
+      </VStack>
     </Center>
   );
 }
