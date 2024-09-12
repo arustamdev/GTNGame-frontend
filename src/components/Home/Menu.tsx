58227c297b03970a76fb29162e8fbcbae09870b8
arustamdev <arustamdev@gmail.com>
2024-09-12 05:59:30 +0200

    added second modal for play button

--- src/components/Home/Menu.tsx
+++ src/components/Home/Menu.tsx
@@ -15,29 +15,60 @@ import {
 import { Link } from 'react-router-dom';
 
 function Menu() {
-  const { isOpen, onOpen, onClose } = useDisclosure();
+  const confirmPlayModal = useDisclosure();
+  const confirmTrainingModal = useDisclosure();
 
   return (
     <>
       <Center h={'60%'}>
         <Container>
           <Stack direction={'column'}>
-            <Button colorScheme="teal">Play</Button>
-            <Button colorScheme="teal" onClick={onOpen}>
+            <Button colorScheme="teal" onClick={confirmPlayModal.onOpen}>
+              Play
+            </Button>
+            <Button colorScheme="teal" onClick={confirmTrainingModal.onOpen}>
               Training mode
             </Button>
           </Stack>
         </Container>
       </Center>
-      <Modal isOpen={isOpen} onClose={onClose} isCentered={true}>
+      <Modal
+        isOpen={confirmTrainingModal.isOpen}
+        onClose={confirmTrainingModal.onClose}
+        isCentered={true}
+      >
         <ModalOverlay />
         <ModalContent alignItems={'center'} m={'10px'}>
           <ModalHeader>Enter training mode?</ModalHeader>
           <ModalFooter>
-            <Button colorScheme="red" mr={3} onClick={onClose}>
+            <Button
+              colorScheme="red"
+              mr={3}
+              onClick={confirmTrainingModal.onClose}
+            >
               No
             </Button>
             <Link to={'/training'}>
+              <Button w={'100%'} colorScheme="green">
+                Yes, continue
+              </Button>
+            </Link>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+      <Modal
+        isOpen={confirmPlayModal.isOpen}
+        onClose={confirmPlayModal.onClose}
+        isCentered={true}
+      >
+        <ModalOverlay />
+        <ModalContent alignItems={'center'} m={'10px'}>
+          <ModalHeader>Find match?</ModalHeader>
+          <ModalFooter>
+            <Button colorScheme="red" mr={3} onClick={confirmPlayModal.onClose}>
+              No
+            </Button>
+            <Link to={'/game'}>
               <Button w={'100%'} colorScheme="green">
                 Yes, continue
               </Button>
