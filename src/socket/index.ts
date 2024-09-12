cdf71585a83f3c23131003491513f5494d75a77f
arustamdev <arustamdev@gmail.com>
2024-09-12 06:00:40 +0200

    added auth data to socket connection

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -1,7 +1,12 @@
+import WebApp from '@twa-dev/sdk';
 import { io } from 'socket.io-client';
 
 const URL = import.meta.env.VITE_BACKEND_URL;
 
-const socket = io(URL);
+const socket = io(URL, {
+  auth: {
+    telegram_data: WebApp.initData,
+  },
+});
 
 export { socket };
